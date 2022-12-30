bd6181817355be12407f40c2fe52a8661df13c27
RedHenDev <neobn@hotmail.com>
2022-12-30 21:46:22 +0000

    refactoring 360, rig ref for data output, fog off

--- galleria_flight_VR/main.js
+++ galleria_flight_VR/main.js
@@ -2,16 +2,24 @@
 Fires a specified event on this entity on a regular timed interval */
 
 AFRAME.registerComponent('log-position-data', {
+	
+				init: function(){
+					//console.log('Hi dad');
+				this.rig=
+				document.querySelector("#rig").object3D;
+				},
+	
         schema: {
           output: {type: 'selector'}
         },
+				
         tick: function () {
 
-          let dataString = "Cam Pos Data:\n"
-          dataString += "Position: \n"
-          dataString += `x: ${this.el.object3D.position.x.toFixed(2)}\n`
-          dataString += `y: ${this.el.object3D.position.y.toFixed(2)}\n`
-          dataString += `z: ${this.el.object3D.position.z.toFixed(2)}\n`
+          let dataString = "Cam Pos Data:\n";
+          dataString += "Position: \n";
+          dataString += `x: ${this.rig.position.x.toFixed(2)}\n`
+          dataString += `y: ${this.rig.position.y.toFixed(2)}\n`
+          dataString += `z: ${this.rig.position.z.toFixed(2)}\n`
 
           dataString += "Orientation: \n"
           dataString += `x: ${this.el.object3D.rotation.x.toFixed(2)}\n`
