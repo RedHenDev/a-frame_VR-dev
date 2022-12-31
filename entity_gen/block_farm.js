41448181dadfd86ccbcf1578418dc04e9ba357ed
RedHenDev <neobn@hotmail.com>
2022-12-31 17:06:04 +0000

    update bracket bug in event listerner

--- entity_gen/block_farm.js
+++ entity_gen/block_farm.js
@@ -2,12 +2,23 @@ AFRAME.registerComponent('farm',{
 	init: function(){
 		// Create a few cubes.
 		this.sceneEl = document.querySelector('a-scene');
-		//pause(1000);
+		
+		const gradientResolution = 16;
+
+		for (let y = 0; y < 32; y++) {
+			let row = '';
+			for (let x = 0; x < 32; x++) {
+				const noise = perlin(x / 8, y / 8);
+				row += noise > 0 ? '#' : ' ';
+			}
+		console.log(row);
+	}
+		
 		for (let i=0;i<1000;i++){
 			let e = document.createElement('a-box');
 			
 			e.object3D.position.x=Math.random()*100-50;
-			e.object3D.position.y=Math.random()*100-50;
+			//e.object3D.position.y=Math.random()*100-50;
 			e.object3D.position.z=Math.random()*100-50;
 			e.object3D.scale.y=Math.random()*3;
 			e.setAttribute('material', 'src:#imgJojo2');
