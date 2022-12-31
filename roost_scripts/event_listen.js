396cf2741fa600f946474843a412211fc3d0428b
RedHenDev <neobn@hotmail.com>
2022-12-31 15:14:32 +0000

    attempt to use perlin noise

--- roost_scripts/event_listen.js
+++ roost_scripts/event_listen.js
@@ -9,4 +9,16 @@ mySphere.addEventListener("mouseenter", e => {
 myPig.addEventListener("fusing", e => {
   myPig.setAttribute("color", "yellow")
  	myPig.object3D.position.z += 1;
+	
+	const gradientResolution = 16;
+
+	for (let y = 0; y < 32; y++) {
+		let row = '';
+		for (let x = 0; x < 32; x++) {
+			const noise = perlin(x / 8, y / 8);
+			row += noise > 0 ? '#' : ' ';
+		}
+	console.log(row);
+}
+	
 });
