779c3f0477b836bb1191266ef059d695d41b482c
RedHenDev <neobn@hotmail.com>
2023-01-01 22:21:31 +0000

    blue loco dev

--- roost_scripts/loco.js
+++ roost_scripts/loco.js
@@ -33,11 +33,11 @@ AFRAME.registerComponent('locomotion', {
 					// Test for speed control.
           if (this.cam.rotation.z > 0 &&
 						 this.cam.rotation < 2.90){
-						this.vel+=0.01;
+						this.vel+=0.001;
 					}
 					else if (this.cam.rotation.z < 0 &&
-									 this.cam.rotation < -2.90){
-						this.vel-=0.01;
+									 this.cam.rotation > -2.90){
+						this.vel-=0.001;
 					}
 					
           let speed=-this.vel;
