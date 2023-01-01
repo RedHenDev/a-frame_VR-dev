599e8e3e3419225af05c4d8f643ad5b5f4433f74
RedHenDev <neobn@hotmail.com>
2023-01-01 23:09:46 +0000

    faster loco dev abs

--- roost_scripts/loco.js
+++ roost_scripts/loco.js
@@ -40,8 +40,8 @@ AFRAME.registerComponent('locomotion', {
 //						this.vel-=0.01;
 //					}
 					let ws=Math.abs(this.cam.rotation.z);
-					this.vel = 3.14-ws;
-          let speed=-this.vel*0.01;
+					this.vel = ws-3.14;
+          let speed=-this.vel*0.1;
 					// Speed cap.
 //					const maxS=0.04;
 //					if (speed < -maxS){
